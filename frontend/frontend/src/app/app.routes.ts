f0a42ca99df66349e2dd96ad92e06b5c3ec49264
stefaniepirhofer <stefanie.pirhofer@outlook.de>
2026-01-27 14:28:08 +0100

    fixed bugs for signup page, now works rudimentary

--- frontend/frontend/src/app/app.routes.ts
+++ frontend/frontend/src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
   { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/signup', loadComponent: () => import('./auth/signup/signup.component').then(m => m.SignupComponent)},
-  { path: 'auth/sighpu/restaurant', loadComponent: () => import('./auth/signup-restaurant/signup-restaurant.component').then(m => m.SignupRestaurantComponent)},
+  { path: 'auth/signup/restaurant', loadComponent: () => import('./auth/signup-restaurant/signup-restaurant.component').then(m => m.SignupRestaurantComponent)},
 
   { path: '**', redirectTo: '' },
 ];
