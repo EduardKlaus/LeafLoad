441268491b1719cdac745d54ad311975a045d1fe
stefaniepirhofer <stefanie.pirhofer@outlook.de>
2026-01-27 12:21:13 +0100

    changed app.routes.ts / added new routes

--- frontend/frontend/src/app/app.routes.ts
+++ frontend/frontend/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/signup', loadComponent: () => import('./auth/signup/signup.component').then(m => m.SignupComponent)},
+  { path: 'auth/sighpu/restaurant', loadComponent: () => import('./auth/signup-restaurant/signup-restaurant.component').then(m => m.SignupRestaurantComponent)},
+
+  { path: '**', redirectTo: '' },
 ];
