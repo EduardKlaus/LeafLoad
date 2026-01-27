c0ba3368df4699cabc9ce4ae79710ae568947d44
stefaniepirhofer <stefanie.pirhofer@outlook.de>
2026-01-27 22:29:55 +0100

    created home page with responsive design

--- frontend/frontend/src/app/home/home.component.ts
+++ frontend/frontend/src/app/home/home.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+type Restaurant = {
+  id: number;
+  name: string;
+  imageUrl?: string | null;
+}
 
 @Component({
   selector: 'app-home',
@@ -8,4 +16,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  restaurants$: Observable<Restaurant[]> | null = null;
+
+  readonly fallbackImage = 'assets/restaurant_fallback.jpg'; // if restaurant doesn't have an image
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.restaurants$ = this.http.get<Restaurant[]>('/restaurants'); // TODO: path anpassen
+  }
+
+  imgSrc(r: Restaurant): string {
+    return (r.imageUrl && r.imageUrl.trim().length > 0) ? r.imageUrl : this.fallbackImage;
+  }
+
+  trackById(_: number, r: Restaurant) {
+    return r.id;
+  }
+}
